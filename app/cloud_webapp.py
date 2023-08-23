46b568c58ef89f041835675b5090f79ddf3ecea7
Boyce Ingrid <boyceing@at-c2.msk.21-school.ru>
2023-08-23 15:15:18 +0300

    app ready

--- app/cloud_webapp.py
+++ app/cloud_webapp.py
@@ -3,21 +3,27 @@ import http.server
 import socketserver
 import socket
 
+
 class RequestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/hostname':
+        if self.path == '/' or self.path == '':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write('hello'.encode())
+        elif self.path == '/hostname':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(socket.gethostname().encode())
         elif self.path == '/author':
-            author = os.environ['AUTHOR']
+            author = os.environ.get('AUTHOR', 'Unknown')
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(author.encode())
         elif self.path == '/id':
-            uuid_value = os.environ['UUID']
+            uuid_value = os.environ.get('UUID', 'Unknown')
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
@@ -25,12 +31,14 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
         else:
             self.send_error(404)
 
+
 def run():
     port = 8000
     handler = RequestHandler
     with socketserver.TCPServer(("", port), handler) as httpd:
         print(f"Server running on port {port}")
         httpd.serve_forever()
+
 
 if __name__ == "__main__":
     run()
