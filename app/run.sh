6ca4ec4d763da86c57101d41140e0b878098b5ee
Boyce Ingrid <boyceing@il-j5.msk.21-school.ru>
2023-08-30 02:05:21 +0300

    run.sh minor upgrade

--- app/run.sh
+++ app/run.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-if [ $1 -eq 1 ]; then
+if [ $# -eq 1 ]; then  # argv mode
+if [ $1 -eq 1 ]; then  # valid argc
 
 docker build -t app:boyceing .
 docker run -d -p 8000:8000 --name boyceing_app app:boyceing
 docker ps -a
+
+sleep 1
+
+echo "The hostname is: $(curl -s http://localhost:8000/hostname)"
+echo "The UUID is: $(curl -s http://localhost:8000/id)"
+echo "The Author is: $(curl -s http://localhost:8000/author)"
 
 fi
 
@@ -14,3 +21,12 @@ docker rm -f boyceing_app
 docker rmi app:boyceing
 
 fi
+
+if [ $1 -eq 3 ]; then
+
+echo "The hostname is: $(curl -s http://localhost:8000/hostname)"
+echo "The UUID is: $(curl -s http://localhost:8000/id)"
+echo "The Author is: $(curl -s http://localhost:8000/author)"
+
+fi  # argv mode
+fi  # valid argc
